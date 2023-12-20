49aff686fd696c3988faa3b7e1b04c56f4c6e5dd
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2023-12-20 21:28:20 +0000

    portfolio updated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,45 @@
+var birthday = 1999
 var date = new Date().getFullYear()
-        var yearSpan = document.getElementById('currentYear')
-        yearSpan.innerHTML = date
+var yearSpan = document.getElementById('currentYear')
+var dynamicAge = document.querySelector('span#dynamicAge')
+var atualAge = date - birthday
+var miniProfileInfoImage = document.getElementById('miniProfileInfoImage')
+var miniProfileInfoName = document.getElementById('miniProfileInfoName')
+var body = this.document.getElementsByTagName('BODY')[0]
+yearSpan.innerHTML = date
 
-        function clickMenu() {
-            var items = document.getElementById('items')
+dynamicAge.innerHTML = atualAge
 
-            if (items.style.display == 'block') {
-                items.style.display = 'none'
-            } else {
-                items.style.display = 'block'
-            }
+window.addEventListener('scroll', function(){
+    const scrollDistance = window.scrollY
+    if (scrollDistance > 770){
+        miniProfileInfoImage.style.display = 'block'
+        miniProfileInfoName.style.display = 'block'
+    } else {
+        miniProfileInfoImage.style.display = 'none'
+        miniProfileInfoName.style.display = 'none'
+    }
+})
+
+
+// functions for the navbar menu
+function clickMenu() {
+    if (window.innerWidth < 990){
+        var items = document.getElementById('items')
+    
+        if (items.style.display == 'block') {
+            items.style.display = 'none'
+        } else {
+            items.style.display = 'block'
         }
+    }
+}
 
-        function sizeChange() {
-            if (window.innerWidth >= 992) {
-                items.style.display = 'block'
-            } else {
-                items.style.display = 'none'
-            }
-        }    
+// function for the body resizing
+function sizeChange() {
+    if (window.innerWidth >= 992) {
+        items.style.display = 'block'
+    } else {
+        items.style.display = 'none'
+    }
+}    
